f8e48e5b913241176ece9da4b54eec1d38339b81
Jake Davis <89550764+jaked689@users.noreply.github.com>
2022-12-08 19:34:21 -0700

    code done possibly

--- examples.cpp
+++ examples.cpp
@@ -5,6 +5,32 @@
 
 using namespace std;
 
+class Calculator{
+    public:
+        virtual int operator()(int a, int b) const{
+            return 0;
+        }
+};
+
+class Add: public Calculator{
+    public:
+    int operator()(int a, int b) const{
+        return a+b;
+    }
+};
+
+class Multiply: public Calculator{
+    public:
+    int operator()(int a, int b) const{
+        return a*b;
+    }
+};
+
+int operate(const Calculator& calc, int a, int b){
+    return calc(a,b);
+}
+
+
 int square_plus_one(int n){
     return n*n+1;
 }
@@ -52,8 +78,17 @@ int main(){
     }
     cout<<endl;
     
+    
+    //classes to overload Calculator's original function
+    Add add;
+    Multiply mult;
+    
+    cout<<"Addition of 2 and 7: "<<operate(add,2,7)<<endl;
+    cout<<"Multiplication of 2 and 7: "<<operate(mult,2,7)<<endl;
+    
     return 0;
 }
 //code references: 
     //https://cplusplus.com/reference/algorithm/remove_if/
     //https://www.modernescpp.com/index.php/higher-order-functions
+    //https://dun.gs/posts/2015-04-17-simple-higher-order-functions.html
